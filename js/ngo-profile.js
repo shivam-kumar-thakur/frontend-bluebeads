fc1a03ab4744426429b79df903bd421b790e9a39
shivam <shivamkrthakur18@gmail.com>
2024-03-12 09:22:43 +0530

    new updates

---
+++ js/ngo-profile.js
@@ -0,0 +1,151 @@
+document.addEventListener("DOMContentLoaded",async function (){
+    await loadbasicInfo();
+})
+
+async function loadbasicInfo(){
+    const responseData={
+        "message": "NGO details retrieved successfully",
+        "data": {
+            "details": {
+                "location": {
+                    "coordinates": [
+                        40.7128,
+                        -74.006
+                    ],
+                    "country": "United States",
+                    "state": "New York",
+                    "postalCode": "10000",
+                    "type": "Point"
+                },
+                "_id": "65d252a9f7c01b65cd7043be",
+                "ngo_name": "ngo1",
+                "ngo_description": "trail ngo",
+                "camps": [],
+                "ngoHeadName": "John Doe",
+                "ngoHeadContact": "+91123456789",
+                "ngoHeadEmail": "john@example.com",
+                "ngoHeadGender": "male",
+                "createdAt": "2024-02-18T18:55:37.650Z",
+                "updatedAt": "2024-02-18T18:55:37.650Z",
+                "__v": 0
+            },
+            "overallData": {
+                "totalcamp": 0,
+                "totalVoulntersRegister": 0,
+                "totalDonorsRegister": 0,
+                "totalPersonsDonated": 0,
+                "bloodGroups": {
+                    "A+": 10,
+                    "A-": 0,
+                    "B+": 20,
+                    "B-": 45,
+                    "AB+": 45,
+                    "AB-": 0,
+                    "O+": 23,
+                    "O-": 45
+                }
+            }
+        },
+        "statuscode": 200,
+        "success": true
+    };    
+    try{
+        // const response=await fetch();
+        // const responseData=await response.json();
+
+        if(responseData.statuscode===400 || responseData.statuscode===401){
+            window.location.href="ngo-profile-details.html";
+        }
+        else if(responseData.statuscode===200){
+            const primary_details=document.getElementById("primary-details-ngo");
+            primary_details.innerHTML=`<p><span class="head-font-2">NGO NAME:</span>${responseData.data.details.ngo_name}</li>
+            <p><span class="head-font-2">NGO Head Name:</span>${responseData.data.details.ngoHeadName}</li>
+            <p><span class="head-font-2">NGO Head Contact:</span>${responseData.data.details.ngoHeadContact}</li>
+            <p><span class="head-font-2">NGO Head Email:</span>${responseData.data.details.ngoHeadEmail}</li>
+            <p><span class="head-font-2">NGO Head Gender:</span>${responseData.data.details.ngoHeadGender}</li>
+            <p><span class="head-font-2">Location:</span>${responseData.data.details.location.state},${responseData.data.details.location.country},${responseData.data.details.location.postalCode}</li>`;
+
+            loadOverallData(responseData);
+            loadStats(responseData);
+        }
+        else{
+            window.location.href="login.html";
+        }
+    }
+    catch(error){
+        console.error("Error : ",error.message);
+    }
+}
+
+function loadOverallData(responseData){
+    document.getElementById("total-camps").innerText=`${responseData.data.overallData.totalcamp}`;
+    document.getElementById("total-vol-reg").innerText=`${responseData.data.overallData.totalVoulntersRegister}`;
+    document.getElementById("total-donor-reg").innerText=`${responseData.data.overallData.totalDonorsRegister}`;
+    document.getElementById("total-people-donated").innerText=`${responseData.data.overallData.totalPersonsDonated}`;
+}
+
+function loadStats(responseData) {
+    // Data for blood group counts (total and donated)
+    let max = 0;
+
+    Object.keys(responseData.data.overallData.bloodGroups).forEach((bloodgroup) => {
+        if (parseInt(responseData.data.overallData.bloodGroups[bloodgroup]) > max) {
+            max = parseInt(responseData.data.overallData.bloodGroups[bloodgroup]);
+        }
+    });
+
+    // Get the tubes container
+    const tubesContainer = document.getElementById("tubes-1");
+
+    // Loop through each blood group data and update the HTML
+    Object.keys(responseData.data.overallData.bloodGroups).forEach(bloodGroup => {
+        // Create a card for each blood group
+        const cardElement = document.createElement("div");
+        cardElement.className = "card";
+
+        const details = document.createElement("div");
+        details.className = "details";
+
+        // Create the tube element for the blood group
+        const tubeElement = document.createElement("div");
+        tubeElement.className = "tube";
+
+        // Set the default percentage and color
+        let percentage = 0;
+        let fillColor = getRandomColor();
+
+        if (max > 0) {
+            percentage = (parseInt(responseData.data.overallData.bloodGroups[bloodGroup]) / max) * 100;
+        }
+
+        tubeElement.style.setProperty("--percent", percentage);
+        tubeElement.style.setProperty("--fill-color", fillColor);
+
+        // Construct the inner HTML of the tube element
+        tubeElement.innerHTML = `
+            <i class="cap"></i>
+            <i class="fill"></i>
+            <div class="base">
+                <div class="icon"><i class="fa-duotone fa-chart-pie-simple"></i></div>
+            </div>`;
+
+        details.innerHTML = `
+            <h3>${bloodGroup}</h3>
+            <p style="font-weight: bold;"><span style="font-weight: bold; color: #940686">Total Units Collected </span>${responseData.data.overallData.bloodGroups[bloodGroup]}</p>`;
+
+        // Append the details element to the card element
+        cardElement.appendChild(details);
+
+        // Append the tube element to the card element
+        cardElement.appendChild(tubeElement);
+
+        // Append the card element to the tubesContainer
+        tubesContainer.appendChild(cardElement);
+    });
+}
+
+// Function to generate random color
+function getRandomColor() {
+    // return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return "#940686";
+}
---
+++ js/user-profile.js
@@ -0,0 +1,237 @@
+document.addEventListener("DOMContentLoaded",async function (){
+    await loadBasisInfo();
+    loadBloodBanks();
+    loadCampInfo();
+})
+
+async function loadBasisInfo(){
+    const responseData={
+        "message": "User details retrieved successfully",
+        "data": {
+            "details": {
+                "_id": "65d258797c2a9af0f3e89996",
+                "userName": "John Doe",
+                "bloodGroup": "O+",
+                "gender": "male",
+                "dob": "1990-01-01T00:00:00.000Z",
+                "email": "john@example.com",
+                "location": {
+                    "type": "Point",
+                    "coordinates": [],
+                    "country": "United States",
+                    "state": "New York",
+                    "postalCode": "10000",
+                    "_id": "65d258797c2a9af0f3e89997"
+                },
+                "lastdonation": "2023-12-25T00:00:00.000Z",
+                "donations": [
+                    // {
+                    //     "campId": "65d2709544488101701d2f93",
+                    //     "requestStatus": 1,
+                    //     "_id": "65d27932b546b626a5721d71"
+                    // }
+                ],
+                "volunteerExperience": [
+                    // {
+                    //     "campId": "65d2709544488101701d2f93",
+                    //     "requestStatus": 1,
+                    //     "_id": "65d27910b546b626a5721d67"
+                    // }
+                ],
+                "requests": [
+                    {
+                        "requestId": "65d26cb675a85846d96ba76a",
+                        "requestStatus": 1,
+                        "_id": "65d26cb775a85846d96ba76d"
+                    }
+                ],
+                "readyToDonate": [],
+                "createdAt": "2024-02-18T19:20:25.456Z",
+                "updatedAt": "2024-02-19T04:40:05.131Z",
+                "__v": 3
+            }
+        },
+        "statuscode": 200,
+        "success": true
+    };
+    
+    try{
+
+        // const response=await fetch("",{
+        //     method:"GET",
+        //     headers:{
+        //         "Content-Tpye":"application/json"
+        //     }
+        // })
+
+        // const responseData=await response.json();
+
+        if(responseData.statuscode===400 || responseData.statuscode===401){
+            window.location.href="user-profile-details.html";
+       }
+
+       else if(responseData.statuscode===200){
+
+            //load data
+            // Update user name
+            document.getElementById("userName").innerText = responseData.data.details.userName;
+
+            // Update primary details
+            const primaryDetailsOverall = document.getElementById("primary-details-overall");
+            const userDetails = responseData.data.details;
+            const userDetailsHTML = `
+                <p><span class="head-font-2">BLOOD GROUP:</span> ${userDetails.bloodGroup}</p>
+                <p><span class="head-font-2">GENDER:</span> ${userDetails.gender}</p>
+                <p><span class="head-font-2">Date of Birth:</span> ${userDetails.dob}</p>
+                <p><span class="head-font-2">EMAIL:</span> ${userDetails.email}</p>
+                <p><span class="head-font-2">LOCATION:</span> ${userDetails.location.state}, ${userDetails.location.country}, ${userDetails.location.postalCode}</p>
+                <p><span class="head-font-2">LAST DONATION:</span> ${userDetails.lastdonation}</p>
+            `;
+            primaryDetailsOverall.innerHTML = userDetailsHTML;
+
+            // load other data
+            loadRequestData(responseData);
+            loadPrimaryInfo(responseData);
+
+       }
+
+       else{
+        window.location.href="login.html";
+       }
+
+    } catch(error){
+        console.error("Error : ",error.message);
+    }
+}
+
+async function loadRequestData(responseData){
+
+    const first_action_group=document.getElementById("first-action-group");
+    const requestDiv=document.createElement("div");
+    requestDiv.classList.add("card","request-donation","primiary-card-info");
+    requestDiv.innerHTML=`<p class="head-font">Find Donor</p>
+    <hr>
+    <!-- <br> -->
+    <p>We are here to help YOU!<br>Find your Donor with A.I. help !</p>
+    <button class="button-2 btn-pur" onclick="location.href = 'find_donor.html';">Find</button>
+    </div>`;
+
+    first_action_group.append(requestDiv);
+
+    for(let i=0;i<responseData.data.details.requests.length;i++){
+        const currentRequest=document.createElement("div");
+        currentRequest.classList.add("card", "request-donation", "primiary-card-info");
+        try{
+            // const responseRequest=await fetch();
+            // const responseRequestData=await responseRequest.json();
+
+            // add all this things.
+            currentRequest.innerHTML=`<p class="head-font">Current Request</p>
+            <hr>
+            <!-- <p>No requests.</p> -->
+            <!-- Request Details -->
+
+            <p><span class="head-font-2">Blood Group Needed:</span>O+</p>
+            <p><span class="head-font-2">LOCATION:</span> Chennai, Tamil Nadu, INDIA 600127</p>
+            <p><span class="head-font-2">Request Status:</span> Open</p>
+             `;
+            first_action_group.append(currentRequest);
+
+        }
+        catch(error){
+            console.error("Error : ",error);
+        }
+
+    }
+
+
+    const previousHistory=document.createElement("div");
+    previousHistory.classList.add("card");
+    previousHistory.classList.add("request-donation");
+    previousHistory.classList.add("primiary-card-info");
+    previousHistory.innerHTML=`<p class="head-font">Previous History</p>
+    <hr>
+    <p>View your previous history of requests.</p>
+    <button class="button-1 btn-or">View</button>`;
+
+    first_action_group.append(previousHistory);
+
+}
+
+
+function loadPrimaryInfo(responseData) {
+    if ((responseData.data.details.donations.length === 0) && (responseData.data.details.volunteerExperience.length === 0)) {
+        const donation_div = document.getElementById("donation-div");
+        const donation_div_innerHTML = `<div class="card request-donation">
+            <p class="head-font">Donations Done</p>
+            <hr>
+            <p>Make your first donation. 1 donation = 4 life.</p>
+            <img class="public-welfare-images" src="images/donate-it.jpg" alt="No Data Found" height="200px">
+            <button class="button-1 btn-or" onclick="location.href = 'find_ngo.html';">Donate</button>
+        </div>
+        <div class="card request-donation">
+            <p class="head-font">Volunteer Experience</p>
+            <hr>
+            <p>Be a volunteer for the donation camp. Find nearby camps.</p>
+            <center>
+                <img class="public-welfare-images" src="images/find-for-volunteer.jpg" alt="No Data Found" height="200px" width="360px">
+            </center>
+            <button class="button-1 btn-or" onclick="location.href = 'find_ngo.html';">Find</button>
+        </div>`;
+
+        donation_div.innerHTML = donation_div_innerHTML;
+    } else {
+        // Handle other conditions based on your requirements
+        const donation_div = document.getElementById("donation-div");
+        const h1 = document.createElement("h1");
+        h1.innerText = "Second condition";
+        donation_div.appendChild(h1);
+    }
+}
+
+
+async function loadBloodBanks(){
+    // const responseBloodbank=await fetch("",{
+    //     method:"GET",
+    //     headers:{
+    //                 "Content-Tpye":"application/json"
+    //             }
+    // });
+    // const responseBloodbankData=await responseBloodbank.json();
+    const blood_banks_div=document.getElementById("blood-banks-div");
+    // if(responseBloodbankData){
+    for(var i=0;i<2;i++){
+        const div=document.createElement("div");
+        div.classList.add("card");
+        div.classList.add("request-donation");
+        div.innerHTML=` <!-- Blood Banks in User's State -->
+                <p><span class="head-font-2">NAME:</span>Athma Blood Centre (ABC) - Blood Bank</p>
+                <p><span class="head-font-2">LOCATION:</span> Chennai, Tamil Nadu, INDIA 600044</p>
+                <p><span class="head-font-2">CONTACT:</span> 095661 54918</p>`;
+        blood_banks_div.append(div);
+    }
+    // }
+}
+
+async function loadCampInfo(){
+    // const responseCamp=await fetch("",{
+    //     method:"GET",
+    //     headers:{
+    //                 "Content-Tpye":"application/json"
+    //             }
+    // });
+    // const responseCampData=await responseCamp.json();
+    const camps_div=document.getElementById("camps-div");
+    // if(responseCampData){
+        const div=document.createElement("div");
+        div.classList.add("card");
+        div.classList.add("request-donation");
+        div.innerHTML=`<!-- camps in user area -->
+                    <p><span class="head-font-2">Camp NAME:</span>Athma Blood Centre (ABC) - Blood Bank</p>
+                    <p><span class="head-font-2">NGO:</span>Athma Blood Centre (ABC) - Blood Bank  </p>
+                    <p><span class="head-font-2">Description:</span>We ensure adequate supply of safe and quality blood / blood products such as - Whole Blood - Packed Red Cells Concentrate (PRBC) - Fresh Frozen Plasma (FFP) - Cryoprecipitate ( Cryo) - Platelet Concentrate (RDP) - Single Donor Platelet (SDP) - Leuco depleted Platelet - Leuco depleted Red Cells - Irradiated processing charges - Red Cells in SAGM to all needy patients at all times, by motivating volunteers to donate blood, and organizing, conducting voluntary blood donation camps in nearby areas.</p>
+                    <p><span class="head-font-2">LOCATION:</span> Chennai, Tamil Nadu, INDIA 600044</p>`;
+        camps_div.appendChild(div);
+    // }
+}
+
