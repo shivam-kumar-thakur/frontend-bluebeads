e569a06da2a89af45e1c56c85324b67bb47da021
shivam <shivamkrthakur18@gmail.com>
2024-03-16 04:41:09 +0530

    updates

--- js/user-profile-details.js
+++ js/user-profile-details.js
@@ -1,26 +1,28 @@
-
 document.getElementById("submit-btn").addEventListener("click", async (event) => {
-    
-    const name = document.getElementById("name").value;
-    const bloodGroup = document.getElementById("blood").value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const location = document.getElementById("location").value;
-    const dob = document.getElementById("dob").value;
-    const email = document.getElementById("email").value;
-    // Construct the data object
-    const data = {
-        userName: name,
-        bloodGroup,
-        gender,
-        location: {
-            latitude: 51.5074, // Replace with actual latitude
-            longitude: -0.1278 // Replace with actual longitude
-        },
-        dob,
-        email,
-        last_donation_date: "" // You may add last donation date if available, otherwise leave it empty
-    };
     try {
+        event.preventDefault(); // Prevent form submission
+        
+        const name = document.getElementById("name").value;
+        const bloodGroup = document.getElementById("blood").value;
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+        const location = document.getElementById("location").value;
+        const dob = document.getElementById("dob").value;
+        const email = document.getElementById("email").value;
+        
+        // Construct the data object
+        const data = {
+            userName: name,
+            bloodGroup,
+            gender,
+            location: {
+                latitude: 51.5074, // Replace with actual latitude
+                longitude: -0.1278 // Replace with actual longitude
+            },
+            dob,
+            email,
+            last_donation_date: "" // You may add last donation date if available, otherwise leave it empty
+        };
+
         // Make POST request to the API endpoint
         const response = await fetch("https://api.bluebeads.shivamkrthakur.in/v1/user/user-details", {
             method: "POST",
@@ -29,11 +31,12 @@ document.getElementById("submit-btn").addEventListener("click", async (event) =>
             },
             body: JSON.stringify(data)
         });
+
         // Handle response
-        const responseData=await response.json();
-        console.log(responseData);
-        if (responseData.ok) {
-            window.location.href="user-profile";
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            window.location.href = "user-profile"; // Redirect to user profile page
         } else {
             console.error("Failed to submit data");
             // Handle error scenario
