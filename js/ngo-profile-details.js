92c10f6d733e1e2e857e0057d0fe15ee736e6d57
shivam <shivamkrthakur18@gmail.com>
2024-03-19 04:08:37 +0530

    updates

---
+++ js/ngo-profile-details.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded",async ()=>{
+    submitDetails();
+});
+
+const submitDetails=async ()=>{
+    document.getElementById("submit").addEventListener("click",async ()=>{
+        const head_name=document.getElementById("head_name").value;
+        const phno=document.getElementById("phno").value;
+        const email=document.getElementById("email").value;
+        const gender=document.getElementById("gender").value;
+        const dob=document.getElementById("dob").value;
+        const ngo_name=document.getElementById("ngo_name").value;
+        const description=document.getElementById("description").value;
+        const address=document.getElementById("address").value;
+
+        const data={
+            // address we are not using.
+            ngo_name,
+            ngo_description:description,
+            "location": {
+                "latitude": "51.5074",
+                "longitude": "-0.1278"
+            },
+            ngo_head:head_name,
+            head_phone:phno,
+            head_email:email,
+            head_address,
+            head_dob:dob,
+            head_gender:gender
+        }
+
+        try{
+            const response=await fetch("https://api.bluebeads.shivamkrthakur.in/v1/ngo/ngo-details",{
+                method:"POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body:JSON.stringify(data)
+            })
+
+            if(response.ok){
+                const responseData=await response.json();
+                console.log(responseData);
+            }
+            else{
+                console.log("something is not correct : ",response);
+            }
+        }
+        catch(error){
+            console.log("something went wrong : ",error.message);
+        }
+
+    })
+};
